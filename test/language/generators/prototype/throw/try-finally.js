51b52316993742ef1f33bac1d8cba2714e125799
Mike Pennisi <mike@mikepennisi.com>
2015-03-25 17:46:45 -0400

    Factor more tests

--- test/language/generators/prototype/throw/try-finally.js
+++ test/language/generators/prototype/throw/try-finally.js
@@ -3,17 +3,19 @@
 /*---
 es6id: 25.3.1.4
 description: >
-    
+    During iteration, the value specified to `throw` should effect control flow
+    as though it were declared at the corresponding location within the
+    generator function body.
 ---*/
 
 function* g() {
   yield 1;
   try {
     yield 2;
-  } catch (e) {
-    yield e;
+  } finally {
+    yield 3;
   }
-  yield 3;
+  yield 4;
 }
 function Sentinel() {}
 var iter, result, exception;
@@ -75,20 +77,14 @@ assert.sameValue(
 assert.sameValue(
   result.done, false, 'Throw within `try`: Second result `done` flag'
 );
-result = iter.throw(exception);
+result = iter.next();
 assert.sameValue(
-  result.value, exception, 'Throw within `try`: Third result `value`'
+  result.value, 3, 'Throw within `try`: Third result `value`'
 );
 assert.sameValue(
   result.done, false, 'Throw within `try`: Third result `done` flag'
 );
-result = iter.next();
-assert.sameValue(
-  result.value, 3, 'Throw within `try`: Fourth result `done` flag'
-);
-assert.sameValue(
-  result.done, false, 'Throw within `try`: Fourth result `value`'
-);
+assert.throws(Sentinel, function() { iter.throw(new Sentinel()); });
 result = iter.next();
 assert.sameValue(
   result.value,
@@ -106,43 +102,43 @@ exception = new Sentinel();
 iter = g();
 result = iter.next();
 assert.sameValue(
-  result.value, 1, 'Throw within `catch`: First result `value`'
+  result.value, 1, 'Throw within `finally`: First result `value`'
 );
 assert.sameValue(
-  result.done, false, 'Throw within `catch`: First result `done` flag'
-);
-result = iter.next();
-assert.sameValue(
-  result.value, 2, 'Throw within `catch`: Second result `value`'
-);
-assert.sameValue(
-  result.done, false, 'Throw within `catch`: Second result `done` flag'
-);
-result = iter.throw(exception);
-assert.sameValue(
-  result.value, exception, 'Throw within `catch`: Third result `value`'
-);
-assert.sameValue(
-  result.done, false, 'Throw within `catch`: Third result `done` flag'
+  result.done, false, 'Throw within `finally`: First result `done` flag'
 );
 result = iter.next();
 assert.sameValue(
-  result.value, 3, 'Throw within `catch`: Fourth result `value`'
+  result.value, 2, 'Throw within `finally`: Second result `value`'
 );
 assert.sameValue(
-  result.done, false, 'Throw within `catch`: Fourth result `done` flag'
+  result.done, false, 'Throw within `finally`: Second result `done` flag'
+);
+result = iter.next();
+assert.sameValue(
+  result.value, 3, 'Throw within `finally`: Third result `value`'
+);
+assert.sameValue(
+  result.done, false, 'Throw within `finally`: Third result `done` flag'
+);
+result = iter.next();
+assert.sameValue(
+  result.value, 4, 'Throw within `finally`: Fourth result `value`'
+);
+assert.sameValue(
+  result.done, false, 'Throw within `finally`: Fourth result `done` flag'
 );
 assert.throws(Sentinel, function() { iter.throw(new Sentinel()); });
 result = iter.next();
 assert.sameValue(
   result.value,
   undefined,
-  'Throw within `catch`: Result `value` is undefined when done'
+  'Throw within `finally`: Result `value` is undefined when done'
 );
 assert.sameValue(
   result.done,
   true,
-  'Throw within `catch`: Result `done` flag is `true` when done'
+  'Throw within `finally`: Result `done` flag is `true` when done'
 );
 iter.next();
 
@@ -150,35 +146,37 @@ exception = new Sentinel();
 iter = g();
 result = iter.next();
 assert.sameValue(
-  result.value, 1, 'Throw following `catch`: First result `value`'
+  result.value, 1, 'Throw following `finally`: First result `value`'
 );
 assert.sameValue(
-  result.done, false, 'Throw following `catch`: First result `done` flag'
+  result.done, false, 'Throw following `finally`: First result `done` flag'
 );
 result = iter.next();
 assert.sameValue(
-  result.value, 2, 'Throw following `catch`: Second result `value`'
+  result.value, 2, 'Throw following `finally`: Second result `value`'
 );
 assert.sameValue(
-  result.done, false, 'Throw following `catch`: Second result `done` flag'
+  result.done, false, 'Throw following `finally`: Second result `done` flag'
 );
 result = iter.throw(exception);
 assert.sameValue(
-  result.value, exception, 'Throw following `catch`: Third result `value`'
+  result.value, 3, 'Throw following `finally`: Third result `value`'
 );
 assert.sameValue(
   result.done,
   false,
-  'Throw following `catch`: Third result `done` flag'
+  'Throw following `finally`: Third result `done` flag'
 );
 assert.throws(Sentinel, function() { iter.throw(new Sentinel()); });
 result = iter.next();
 assert.sameValue(
-  result.value, undefined, 'Result `value` is undefined when done'
+  result.value,
+  undefined,
+  'Throw following `finally`: Result `value` is undefined when done'
 );
 assert.sameValue(
   result.done,
   true,
-  'Throw following `catch`: Result `done` flag is `true` when done'
+  'Throw following `finally`: Result `done` flag is `true` when done'
 );
 iter.next();
