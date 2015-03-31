39f25a1136e8de5de8ecaa5245737fab8cb9fd5a
Mike Pennisi <mike@mikepennisi.com>
2015-03-31 13:35:04 -0400

    Factor more tests

---
+++ test/language/templates/template-values/no-substitution-empty.js
@@ -0,0 +1,16 @@
+// Copyright (C) Copyright 2014 the V8 project authors. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+/*---
+es6id: 11.8.6
+description: >
+    The TV and TRV of NoSubstitutionTemplate :: `` is the empty code unit
+    sequence.
+---*/
+
+var calls = 0;
+(function(s) {
+  calls++;
+  assert.sameValue(s[0], '', 'Template value');
+  assert.sameValue(s.raw[0], '', 'Template raw value');
+})``;
+assert.sameValue(calls, 1);
---
+++ test/language/templates/template-values/template-head-content.js
@@ -0,0 +1,15 @@
+// Copyright (C) Copyright 2014 the V8 project authors. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+/*---
+es6id: 11.8.6
+description: >
+    The TV of TemplateHead :: ` TemplateCharacters ${ is the TV of
+    TemplateCharacters.
+---*/
+
+var calls = 0;
+(function(s) {
+  calls++;
+  assert.sameValue(s[0], 'foo', 'Template value');
+})`foo${1}`;
+assert.sameValue(calls, 1);
