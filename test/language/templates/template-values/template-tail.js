dd1ce4353c8f02ae468c5588446ea69f9fe4614e
Mike Pennisi <mike@mikepennisi.com>
2015-03-31 13:54:13 -0400

    Re-organize

---
+++ test/language/templates/template-values/template-tail.js
@@ -0,0 +1,26 @@
+// Copyright (C) Copyright 2014 the V8 project authors. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+/*---
+es6id: 11.8.6
+description: >
+    The TV and TRV of TemplateTail :: }` is the empty code unit sequence.
+    The TV of TemplateTail :: } TemplateCharacters ` is the TV of
+    TemplateCharacters.
+---*/
+
+var calls;
+
+calls = 0;
+(function(s) {
+  calls++;
+  assert.sameValue(s[1], '', 'Template value (empty)');
+  assert.sameValue(s.raw[1], '', 'Template raw value (empty)');
+})`${1}`;
+assert.sameValue(calls, 1);
+
+calls = 0;
+(function(s) {
+  calls++;
+  assert.sameValue(s[1], 'foo', 'Template value (with content)');
+})`${1}foo`;
+assert.sameValue(calls, 1);
