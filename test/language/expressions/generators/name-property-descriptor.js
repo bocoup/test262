c89e91bbf46fff26092e1f046341050b890aa160
Mike Pennisi <mike@mikepennisi.com>
2015-04-14 17:23:04 -0400

    Add analogous tests
    Introduce additional tests to increase parity in coverage between
    generator expressions and generator statements.

---
+++ test/language/expressions/generators/name-property-descriptor.js
@@ -0,0 +1,15 @@
+// Copyright (C) 2015 the V8 project authors. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+description: >
+    Generator functions should define a `name` property.
+includes: [propertyHelper.js]
+es6id: 25.2.4
+---*/
+
+var g = function*() {};
+
+verifyNotEnumerable(g, 'name');
+verifyNotWritable(g, 'name');
+verifyConfigurable(g, 'name');
