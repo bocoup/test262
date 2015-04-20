ff0b614e4e6d4f90a0e9c7d4dac6841fdb241536
Mike Pennisi <mike@mikepennisi.com>
2015-04-20 16:36:52 -0400

    fixup! Factor tests

---
+++ test/built-ins/GeneratorPrototype/throw/try-catch-within-catch.js
@@ -0,0 +1,50 @@
+// Copyright (C) Copyright 2013 the V8 project authors. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+/*---
+es6id: 25.3.1.4
+description: >
+    During iteration, the value specified to `throw` should effect control flow
+    as though it were declared at the corresponding location within the
+    generator function body.
+---*/
+
+function* g() {
+  yield 1;
+  try {
+    yield 2;
+    throw exception;
+  } catch (e) {
+    yield e;
+  }
+  yield 3;
+}
+function Sentinel() {}
+var iter, result, exception;
+
+exception = new Sentinel();
+iter = g();
+
+result = iter.next();
+assert.sameValue(result.value, 1, 'First result `value`');
+assert.sameValue(result.done, false, 'First result `done` flag');
+
+result = iter.next();
+assert.sameValue(result.value, 2, 'Second result `value`');
+assert.sameValue(result.done, false, 'Second result `done` flag');
+
+result = iter.next();
+assert.sameValue(result.value, exception, 'Third result `value`');
+assert.sameValue(result.done, false, 'Third result `done` flag');
+
+result = iter.next();
+assert.sameValue(result.value, 3, 'Fourth result `value`');
+assert.sameValue(result.done, false, 'Fourth result `done` flag');
+assert.throws(Sentinel, function() { iter.throw(new Sentinel()); });
+
+result = iter.next();
+assert.sameValue(
+  result.value, undefined, 'Result `value` is undefined when done'
+);
+assert.sameValue(result.done, true, 'Result `done` flag is `true` when done');
+
+iter.next();
