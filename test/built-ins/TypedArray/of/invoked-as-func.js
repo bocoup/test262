279e97b2f20e94706df23157e78b1fda5342f08d
Leonardo Balter <leonardo.balter@gmail.com>
2016-01-14 18:06:24 -0500

    Refine info for TypedArray.of tests

--- test/built-ins/TypedArray/of/invoked-as-func.js
+++ test/built-ins/TypedArray/of/invoked-as-func.js
@@ -1,11 +1,16 @@
-// Copyright (C) Copyright 2015 the V8 project authors. All rights reserved.
+// Copyright (C) Copyright 2016 the V8 project authors. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 /*---
 es6id: 22.2.2.2
 description: >
-    "of" cannot be invoked as a function
+  "of" cannot be invoked as a function
 info: >
-    If IsConstructor(C) is false, throw a TypeError exception.
+  22.2.2.2 %TypedArray%.of ( ...items )
+
+  ...
+  3. Let C be the this value.
+  4. If IsConstructor(C) is false, throw a TypeError exception.
+  ...
 features: [TypedArray]
 includes: [testTypedArray.js]
 ---*/
