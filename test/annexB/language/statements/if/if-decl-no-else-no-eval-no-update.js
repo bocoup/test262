7b47665d8a60e32075c3b5fef60ed38cb28fe224
Mike Pennisi <mike@mikepennisi.com>
2016-03-03 15:05:20 -0500

    Add more tests

---
+++ test/annexB/language/statements/if/if-decl-no-else-no-eval-no-update.js
@@ -0,0 +1,24 @@
+// Copyright (C) 2016 the V8 project authors. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+/*---
+es6id: B.3.4
+esid: sec-functiondeclarations-in-ifstatement-statement-clauses
+description: >
+    Variable binding value is not updated when evaluation does not occur
+info: |
+    The semantics of such a synthetic BlockStatement includes the web legacy
+    compatibility semantics specified in B.3.3.
+
+    B.3.3.1 Changes to FunctionDeclarationInstantiation
+
+    [...]
+    3. When the FunctionDeclaration f is evaluated, perform the following steps
+       in place of the FunctionDeclaration Evaluation algorithm provided in
+       14.1.21:
+       [...]
+flags: [noStrict]
+---*/
+
+if (false) function f() {}
+
+assert.sameValue(typeof f, 'undefined');
---
+++ test/annexB/language/statements/if/if-stmt-else-decl-no-eval-init.js
@@ -0,0 +1,23 @@
+// Copyright (C) 2016 the V8 project authors. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+/*---
+es6id: B.3.4
+esid: sec-functiondeclarations-in-ifstatement-statement-clauses
+description: >
+    Variable binding is initialized to `undefined` in outer scope when
+    declaration is not evaluated
+info: |
+    The semantics of such a synthetic BlockStatement includes the web legacy
+    compatibility semantics specified in B.3.3.
+
+    B.3.3.1 Changes to FunctionDeclarationInstantiation
+
+    [...]
+    2. If instantiatedVarNames does not contain F, then
+       [...]
+flags: [noStrict]
+---*/
+
+assert.sameValue(f, undefined);
+
+if (true) ; else function f() {}
