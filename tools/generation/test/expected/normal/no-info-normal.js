9f800c0cd46d700f61b2237038065690c677ea61
Mike Pennisi <mike@mikepennisi.com>
2016-03-08 11:04:26 -0500

    fixup! Introduce generation tool
    Make "info" field optional

---
+++ tools/generation/test/expected/normal/no-info-normal.js
@@ -0,0 +1,11 @@
+// This file was procedurally generated from the following sources:
+// - tools/generation/test/fixtures/normal.case
+// - tools/generation/test/fixtures/normal/no-info.template
+/*---
+description: foobar (First template name)
+es6id: 1.2.3
+info: >
+    case info
+---*/
+
+First value
