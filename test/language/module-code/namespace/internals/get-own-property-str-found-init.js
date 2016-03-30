59a32a0c8914a719e6d4cb5ea3fef52c2834b041
Mike Pennisi <mike@mikepennisi.com>
2016-03-30 16:52:09 -0400

    Extend test

--- test/language/module-code/namespace/internals/get-own-property-str-found-init.js
+++ test/language/module-code/namespace/internals/get-own-property-str-found-init.js
@@ -16,19 +16,30 @@ flags: [module]
 ---*/
 
 import * as ns from './get-own-property-str-found-init.js';
-export var local = 201;
-export { local as indirect } from './get-own-property-str-found-init.js';
+export var local1 = 201;
+var local2 = 207;
+export { local2 as renamed };
+export { local1 as indirect } from './get-own-property-str-found-init.js';
 export default 302;
 var desc;
 
 assert.sameValue(
-  Object.prototype.hasOwnProperty.call(ns, 'local'), true
+  Object.prototype.hasOwnProperty.call(ns, 'local1'), true
 );
-desc = Object.getOwnPropertyDescriptor(ns, 'local');
+desc = Object.getOwnPropertyDescriptor(ns, 'local1');
 assert.sameValue(desc.value, 201);
-assert.sameValue(desc.enumerable, true, 'local enumerable');
-assert.sameValue(desc.writable, true, 'local writable');
-assert.sameValue(desc.configurable, false, 'local configurable');
+assert.sameValue(desc.enumerable, true, 'local1 enumerable');
+assert.sameValue(desc.writable, true, 'local1 writable');
+assert.sameValue(desc.configurable, false, 'local1 configurable');
+
+assert.sameValue(
+  Object.prototype.hasOwnProperty.call(ns, 'renamed'), true
+);
+desc = Object.getOwnPropertyDescriptor(ns, 'renamed');
+assert.sameValue(desc.value, 207);
+assert.sameValue(desc.enumerable, true, 'renamed enumerable');
+assert.sameValue(desc.writable, true, 'renamed writable');
+assert.sameValue(desc.configurable, false, 'renamed configurable');
 
 assert.sameValue(
   Object.prototype.hasOwnProperty.call(ns, 'indirect'), true
@@ -47,4 +58,3 @@ assert.sameValue(desc.default, 302);
 assert.sameValue(desc.enumerable, true, 'default enumerable');
 assert.sameValue(desc.writable, true, 'default writable');
 assert.sameValue(desc.configurable, false, 'default configurable');
-
