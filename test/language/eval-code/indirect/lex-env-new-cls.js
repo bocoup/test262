b3f53ae65d316249465b433f6e64768d8cc117af
Mike Pennisi <mike@mikepennisi.com>
2016-04-07 18:18:19 -0400

    fixup! Add tests for eval with lexical bindings
    Remov unnecessary semicolons

--- test/language/eval-code/indirect/lex-env-new-cls.js
+++ test/language/eval-code/indirect/lex-env-new-cls.js
@@ -20,14 +20,14 @@ class outside {}
 (0,eval)('class outside {}');
 (0,eval)('"use strict"; class outside {}');
 
-(0,eval)('class xNonStrict {};');
+(0,eval)('class xNonStrict {}');
 
 assert.sameValue(typeof xNonStrict, 'undefined');
 assert.throws(ReferenceError, function() {
   xNonStrict;
 });
 
-(0,eval)('"use strict"; class xStrict {};');
+(0,eval)('"use strict"; class xStrict {}');
 
 assert.sameValue(typeof xStrict, 'undefined');
 assert.throws(ReferenceError, function() {
