49f3c3af7c3a65b56604796ac46acdeae65e100f
Mike Pennisi <mike@mikepennisi.com>
2016-05-09 15:08:45 -0400

    Add tests for __define{G,S}etter__

---
+++ test/annexB/built-ins/Object/prototype/__defineGetter__/define-new.js
@@ -0,0 +1,31 @@
+// Copyright (C) 2016 the V8 project authors. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+/*---
+esid: sec-additional-properties-of-the-object.prototype-object
+description: Behavior when propery exists and is configurable
+info: |
+    [...]
+    3. Let desc be PropertyDescriptor{[[Set]]: setter, [[Enumerable]]: true,
+       [[Configurable]]: true}.
+    4. Let key be ? ToPropertyKey(P).
+    5. Perform ? DefinePropertyOrThrow(O, key, desc).
+    6. Return undefined.
+features: [Symbol]
+includes: [propertyHelper.js]
+---*/
+
+var subject = {};
+var get = function() {};
+var desc, result;
+
+result = subject.__defineGetter__('stringAcsr', get);
+
+desc = Object.getOwnPropertyDescriptor(subject, 'stringAcsr');
+verifyEnumerable(subject, 'stringAcsr');
+verifyConfigurable(subject, 'stringAcsr');
+
+assert.sameValue(desc.get, get, 'descriptor `get` method');
+assert.sameValue(desc.set, undefined, 'descriptor `set` method');
+assert.sameValue(desc.value, undefined, 'descriptor `value` property');
+
+assert.sameValue(result, undefined, 'method return value');
