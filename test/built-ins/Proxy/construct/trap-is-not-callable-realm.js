ecf95bcd853a842582123aaf60f1d69cbb308cc4
Mike Pennisi <mike@mikepennisi.com>
2016-05-18 17:57:11 -0400

    Begin adding tests for Proxy internal methods

---
+++ test/built-ins/Proxy/construct/trap-is-not-callable-realm.js
@@ -0,0 +1,19 @@
+// Copyright (C) 2015 the V8 project authors. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+/*---
+esid: sec-proxy-object-internal-methods-and-internal-slots-construct-argumentslist-newtarget
+es6id: 9.5.14
+description: >
+  Throws if trap is not callable (honoring the Realm of the current execution
+  context)
+includes: [realm.js]
+---*/
+
+var OProxy = $.createRealm().global.Proxy;
+var p = new OProxy(function() {}, {
+  construct: {}
+});
+
+assert.throws(TypeError, function() {
+  new p();
+});
