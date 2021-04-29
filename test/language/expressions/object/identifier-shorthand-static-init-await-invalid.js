91d8d7bb8f943ae94962633860157231b943c675
Mike Pennisi <mike@mikepennisi.com>
2021-04-29 12:38:27 -0400

    fixup! Add tests for "Class Static Init. Blocks" proposal
    Correct justification

--- test/language/expressions/object/identifier-shorthand-static-init-await-invalid.js
+++ test/language/expressions/object/identifier-shorthand-static-init-await-invalid.js
@@ -2,15 +2,14 @@
 // This code is governed by the BSD license found in the LICENSE file.
 /*---
 esid: sec-identifiers
-description: BindingIdentifier may not be `await` within class static blocks
+description: IdentifierReference may not be `await` within class static blocks
 info: |
-  BindingIdentifier : Identifier
+  IdentifierReference : Identifier
 
-  [...]
   - It is a Syntax Error if the code matched by this production is nested,
     directly or indirectly (but not crossing function or static initialization
     block boundaries), within a ClassStaticBlock and the StringValue of
-    Identifier is "await".
+    Identifier is "arguments" or "await".
 negative:
   phase: parse
   type: SyntaxError
