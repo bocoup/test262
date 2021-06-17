d8a159922f353dcaf320820d041a01f70db9e23a
Mike Pennisi <mike@mikepennisi.com>
2021-06-16 20:11:30 -0400

    Add tests for ValidateTypedArray
    The files in this patch are highly similar. Only the test for
    `TypedArray.prototype.copyWithin` was written manually. The others were
    generated from that test via the following script:
    
        #!/bin/bash
    
        set -eu
    
        names_cb='
        every
        filter
        find
        findIndex
        forEach
        map
        reduce
        reduceRight
        some
        '
        names_num='
        at
        fill
        includes
        indexOf
        join
        lastIndexOf
        slice
        '
        names_none='
        entries
        values
        keys
        reverse
        sort
        toLocaleString
        values
        '
    
        for name in $(printf "${names_cb} ${names_num} ${names_none}"); do
          lower=$(echo ${name} | tr '[:upper:]' '[:lower:]')
          if echo "$names_cb" | grep -xq $name; then
            value='() => {}'
          elif echo "$names_num" | grep -xq $name; then
            value='0'
          else
            value=''
          fi
    
          sed \
            -e "s/copywithin/${lower}/g" \
            -e "s/copyWithin/${name}/g" \
            -e "s/${name}(.*);/${name}(${value});/g" \
            ./test/built-ins/TypedArray/prototype/copyWithin/return-abrupt-from-this-oob.js \
            > ./test/built-ins/TypedArray/prototype/${name}/return-abrupt-from-this-oob.js
        done

---
+++ test/built-ins/TypedArray/prototype/sort/return-abrupt-from-this-oob.js
@@ -0,0 +1,39 @@
+// Copyright (C) 2021 the V8 project authors. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+/*---
+esid: sec-%typedarray%.prototype.sort
+description: Return abrupt when "this" value fails buffer boundary checks
+includes: [testTypedArray.js]
+features: [TypedArray, TypedArray.prototype.at, resizable-arraybuffer]
+---*/
+
+assert.sameValue(
+  typeof TypedArray.prototype.sort,
+  'function',
+  'implements TypedArray.prototype.sort'
+);
+
+assert.sameValue(
+  typeof ArrayBuffer.prototype.resize,
+  'function',
+  'implements ArrayBuffer.prototype.resize'
+);
+
+testWithTypedArrayConstructors(TA => {
+  var BPE = TA.BYTES_PER_ELEMENT;
+  var ab = new ArrayBuffer(BPE * 4, {maxByteLength: BPE * 4});
+  var array = new TA(ab, 0, 4);
+
+  try {
+    ab.resize(BPE * 3);
+  } catch (_) {
+    // The host is permitted to fail any "resize" operation at its own
+    // discretion. If that occurs, the semantics that this test is designed to
+    // verify cannot be observed, so bail out.
+    return;
+  }
+
+  assert.throws(TypeError, () => {
+    array.sort();
+  });
+});
