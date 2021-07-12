7a4f9569f5d0431754540a0332a24468e8d597e2
Mike Pennisi <mike@mikepennisi.com>
2021-07-12 18:17:38 -0400

    Remove duplicated assertions

--- test/built-ins/Temporal/now/plainDateTime/return-value.js
+++ test/built-ins/Temporal/now/plainDateTime/return-value.js
@@ -8,34 +8,14 @@ includes: [compareArray.js]
 features: [Temporal]
 ---*/
 
-const actual = [];
-const expected = [
-  "has timeZone.timeZone",
-  "get timeZone.getOffsetNanosecondsFor",
-  "call timeZone.getOffsetNanosecondsFor",
-];
-
-const timeZone = new Proxy({
-  getOffsetNanosecondsFor(instant) {
-    actual.push("call timeZone.getOffsetNanosecondsFor");
-    return -Number(instant.epochNanoseconds % 86400_000_000_000n);
-  },
-}, {
-  has(target, property) {
-    actual.push(`has timeZone.${property}`);
-    return property in target;
-  },
-  get(target, property) {
-    actual.push(`get timeZone.${property}`);
-    return target[property];
-  },
-});
-
 const calendar = Temporal.Calendar.from("iso8601");
+const timeZone = {
+  getOffsetNanosecondsFor(instant) {
+    return -Number(instant.epochNanoseconds % 86400_000_000_000n);
+  }
+};
 
 const result = Temporal.now.plainDateTime(calendar, timeZone);
 for (const property of ["hour", "minute", "second", "millisecond", "microsecond", "nanosecond"]) {
   assert.sameValue(result[property], 0, property);
 }
-
-assert.compareArray(actual, expected);
