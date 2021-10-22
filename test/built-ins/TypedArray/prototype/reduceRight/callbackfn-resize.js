b3de1c8f6b64294f84dac745a68276d1981e8d7d
Mike Pennisi <mike@mikepennisi.com>
2021-10-22 19:34:55 -0400

    Add tests

---
+++ test/built-ins/TypedArray/prototype/reduceRight/callbackfn-resize.js
@@ -0,0 +1,59 @@
+// Copyright (C) 2021 the V8 project authors. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+/*---
+esid: sec-%typedarray%.prototype.reduceright
+description: Instance buffer can be resized during iteration
+includes: [testTypedArray.js, compareArray.js]
+features: [TypedArray, resizable-arraybuffer]
+---*/
+
+testWithTypedArrayConstructors(function(TA) {
+  var BPE = TA.BYTES_PER_ELEMENT;
+  var buffer = new ArrayBuffer(BPE * 3, {maxByteLength: BPE * 3});
+  var sample = new TA(buffer);
+  var prevs, nexts, indices, arrays, result;
+
+  prevs = [];
+  nexts = [];
+  indices = [];
+  arrays = [];
+  result = sample.reduceRight(function(prev, next, index, array) {
+    if (prevs.length === 0) {
+      buffer.resize(BPE);
+    }
+
+    prevs.push(prev);
+    nexts.push(next);
+    indices.push(index);
+    arrays.push(array);
+    return index;
+  }, 262);
+
+  assert.compareArray(prevs, [262, 2, 1], 'prevs (shrink)');
+  assert.compareArray(nexts, [0, undefined, 0], 'nexts (shrink)');
+  assert.compareArray(indices, [2, 1, 0], 'indices (shrink)');
+  assert.compareArray(arrays, [sample, sample, sample], 'arrays (shrink)');
+  assert.sameValue(result, 0, 'result (shrink)');
+
+  prevs = [];
+  nexts = [];
+  indices = [];
+  arrays = [];
+  result = sample.reduceRight(function(prev, next, index, array) {
+    if (prevs.length === 0) {
+      buffer.resize(2 * BPE);
+    }
+
+    prevs.push(prev);
+    nexts.push(next);
+    indices.push(index);
+    arrays.push(array);
+    return index;
+  }, 262);
+
+  assert.compareArray(prevs, [262], 'prevs (grow)');
+  assert.compareArray(nexts, [0], 'nexts (grow)');
+  assert.compareArray(indices, [0], 'indices (grow)');
+  assert.compareArray(arrays, [sample], 'arrays (grow)');
+  assert.sameValue(result, 0, 'result (grow)');
+});
