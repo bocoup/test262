79b1ed4a9cc3a5217110ca413b4c2bba010e4cb7
Mike Pennisi <mike@mikepennisi.com>
2021-11-05 14:46:49 -0400

    Update Array prototype method tests

--- test/built-ins/Array/prototype/find/callbackfn-resize-arraybuffer.js
+++ test/built-ins/Array/prototype/find/callbackfn-resize-arraybuffer.js
@@ -7,10 +7,17 @@ includes: [testTypedArray.js, compareArray.js]
 features: [TypedArray, resizable-arraybuffer]
 ---*/
 
+// If the host chooses to throw as allowed by the specification, the observed
+// behavior will be identical to the case where `ArrayBuffer.prototype.resize`
+// has not been implemented. The following assertion prevents this test from
+// passing in runtimes which have not implemented the method.
+assert.sameValue(typeof ArrayBuffer.prototype.resize, 'function');
+
 testWithTypedArrayConstructors(function(TA) {
   var BPE = TA.BYTES_PER_ELEMENT;
-  var buffer = new ArrayBuffer(BPE * 3, {maxByteLength: BPE * 3});
+  var buffer = new ArrayBuffer(BPE * 3, {maxByteLength: BPE * 4});
   var sample = new TA(buffer);
+  var finalElement, expectedElements, expectedIndices, expectedArrays;
   var elements, indices, arrays, result;
 
   elements = [];
@@ -18,7 +25,18 @@ testWithTypedArrayConstructors(function(TA) {
   arrays = [];
   result = Array.prototype.find.call(sample, function(element, index, array) {
     if (elements.length === 0) {
-      buffer.resize(2 * BPE);
+      try {
+        buffer.resize(2 * BPE);
+        finalElement = undefined;
+        expectedElements = [0, 0];
+        expectedIndices = [0, 1];
+        expectedArrays = [sample, sample];
+      } catch (_) {
+        finalElement = 0;
+        expectedElements = [0, 0, 0];
+        expectedIndices = [0, 1, 2];
+        expectedArrays = [sample, sample, sample];
+      }
     }
 
     elements.push(element);
@@ -27,7 +45,7 @@ testWithTypedArrayConstructors(function(TA) {
     return false;
   });
 
-  assert.compareArray(elements, [0, 0, undefined], 'elements (shrink)');
+  assert.compareArray(elements, [0, 0, finalElement], 'elements (shrink)');
   assert.compareArray(indices, [0, 1, 2], 'indices (shrink)');
   assert.compareArray(arrays, [sample, sample, sample], 'arrays (shrink)');
   assert.sameValue(result, undefined, 'result (shrink)');
@@ -37,7 +55,9 @@ testWithTypedArrayConstructors(function(TA) {
   arrays = [];
   result = Array.prototype.find.call(sample, function(element, index, array) {
     if (elements.length === 0) {
-      buffer.resize(3 * BPE);
+      try {
+        buffer.resize(4 * BPE);
+      } catch (_) {}
     }
 
     elements.push(element);
@@ -46,8 +66,8 @@ testWithTypedArrayConstructors(function(TA) {
     return false;
   });
 
-  assert.compareArray(elements, [0, 0], 'elements (grow)');
-  assert.compareArray(indices, [0, 1], 'indices (grow)');
-  assert.compareArray(arrays, [sample, sample], 'arrays (grow)');
+  assert.compareArray(elements, expectedElements, 'elements (grow)');
+  assert.compareArray(indices, expectedIndices, 'indices (grow)');
+  assert.compareArray(arrays, expectedArrays, 'arrays (grow)');
   assert.sameValue(result, undefined, 'result (grow)');
 });
