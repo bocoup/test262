eabec37b52557daa4914d2f486b37239dcb7c4a6
Mike Pennisi <mike@mikepennisi.com>
2025-09-09 17:47:12 -0400

    Refactor for collaboration

--- generate.py
+++ generate.py
@@ -3,45 +3,12 @@ import os
 import re
 import yaml
 
-features = [
-  ['array-copywithin', '**/copyWithin'],
-  ['array-fill', '**/fill'],
+features = []
+sources = ['./generate-source-1.yml', './generate-source-2.yml']
 
-  # Should include:
-  # - test/built-ins/TypedArray/prototype/findIndex
-  # - test/built-ins/TypedArray/prototype/find/BigInt
-  # - test/built-ins/TypedArrayConstructors/prototype/find
-  # Should NOT include:
-  # - test/built-ins/Iterator/prototype/find
-  # - test/built-ins/TypeArray/prototype/findLast
-  #['array-find', '.*/(Array|TypedArray|TypedArrayConstructors)/.*/(find|findIndex)(/|$)'],
-  ['array-find', '**(Array|TypedArray|TypedArrayConstructors)**(find|findIndex)**'],
-
-  # Test262 maintains a "features" flag named `Array.prototype.includes`, but
-  # is too expansive for this purpose--used to label tests that incidentally
-  # rely on the API without meaningfully verifying its semantics (e.g.
-  # `test/intl402/Intl/supportedValuesOf/collations.js`)
-  ['array-includes', '**Array/prototype/includes**'],
-
-  # Description inadequate; inferred from source:
-  # web-features/features/array-iteration-methods.yml
-  ['array-iteration-methods', '**Array**(every|filter|forEach|indexOf|lastIndexOf|map|reduce|reduceRight|some)**'],
-
-  ['array-iterators', '**Array**(Symbol.iterator|entries|keys|values)**'],
-  ['array-splice', '**Array**splice**'],
-
-  # Should include
-  # - test/annexB/built-ins/Array/from
-  # - test/annexB/built-ins/TypedArrayConstructors/from
-  ['array-from', '**(Array|TypedArray|TypedArrayConstructors)**from**'],
-
-  ['array-isarray', '**isArray**'],
-  ['array-of', '**(Array|TypedArray|TypedArrayConstructors)**of**'],
-
-  ['async-functions', '**(async-function|async-arrow-function|async-method|async-method-static)**'],
-  ['async-functions', '**await', '!async-iteration'],
-  ['async-functions', '**method-definition', 'async-functions', '!class-methods-private'],
-]
+for source in sources:
+    with open(source, 'r') as f:
+        features.extend(yaml.safe_load(f))
 
 for feature in features:
     feature[1] = re.sub('/', os.path.sep, feature[1])
